2824596dc159d503d801a39acd4ad49044d9b217
Code Sandbox <hello@codesandbox.io>
2021-11-27 07:51:36 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.e7a78791f2d0a9c3850c3d895216aa7c.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f97654d66f45fc10d139a7238caa5e31",
+    "url": "https://mukundakamatham.github.io/csb-wxpur/index.html"
+  },
+  {
+    "revision": "801f433d36a35d32f2b5",
+    "url": "https://mukundakamatham.github.io/csb-wxpur/static/css/3.df879eac.chunk.css"
+  },
+  {
+    "revision": "6922bda447af38297a1d",
+    "url": "https://mukundakamatham.github.io/csb-wxpur/static/css/4.e91a4b36.chunk.css"
+  },
+  {
+    "revision": "803b511a764993084257",
+    "url": "https://mukundakamatham.github.io/csb-wxpur/static/css/main.1aa2fabb.chunk.css"
+  },
+  {
+    "revision": "51c31101671d6df14159",
+    "url": "https://mukundakamatham.github.io/csb-wxpur/static/js/2.5428a9c9.chunk.js"
+  },
+  {
+    "revision": "408f8ade359af8695a328ed937291fdf",
+    "url": "https://mukundakamatham.github.io/csb-wxpur/static/js/2.5428a9c9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "801f433d36a35d32f2b5",
+    "url": "https://mukundakamatham.github.io/csb-wxpur/static/js/3.d5369665.chunk.js"
+  },
+  {
+    "revision": "6922bda447af38297a1d",
+    "url": "https://mukundakamatham.github.io/csb-wxpur/static/js/4.4b7a9cd9.chunk.js"
+  },
+  {
+    "revision": "803b511a764993084257",
+    "url": "https://mukundakamatham.github.io/csb-wxpur/static/js/main.6142aaed.chunk.js"
+  },
+  {
+    "revision": "466aa85d5362f35724f3",
+    "url": "https://mukundakamatham.github.io/csb-wxpur/static/js/runtime-main.0b4406e9.js"
+  },
+  {
+    "revision": "a675daac4f528736c4f3d184b4c4eadc",
+    "url": "https://mukundakamatham.github.io/csb-wxpur/static/media/AMCCOLLEGELOGO.a675daac.png"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "https://mukundakamatham.github.io/csb-wxpur/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "https://mukundakamatham.github.io/csb-wxpur/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "c59720d63dc3595a12e1c7e641a1bfa5",
+    "url": "https://mukundakamatham.github.io/csb-wxpur/static/media/airbnbLogo.c59720d6.png"
+  },
+  {
+    "revision": "51cf7192327488da3d2a171280680c16",
+    "url": "https://mukundakamatham.github.io/csb-wxpur/static/media/contactMailDark.51cf7192.svg"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "https://mukundakamatham.github.io/csb-wxpur/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "8ddf76a14a2e3ad3ba62b46d49a75a74",
+    "url": "https://mukundakamatham.github.io/csb-wxpur/static/media/facebookLogo.8ddf76a1.png"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "https://mukundakamatham.github.io/csb-wxpur/static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "ccb25528b2a0436797a252c3ecd1c665",
+    "url": "https://mukundakamatham.github.io/csb-wxpur/static/media/python.ccb25528.jfif"
+  },
+  {
+    "revision": "4b2938b9f5ea2f1ddbd45dccaff628bc",
+    "url": "https://mukundakamatham.github.io/csb-wxpur/static/media/quoraLogo.4b2938b9.png"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "https://mukundakamatham.github.io/csb-wxpur/static/media/skill.34409398.svg"
+  },
+  {
+    "revision": "13780cdd0144cacdef25486e2c2503f8",
+    "url": "https://mukundakamatham.github.io/csb-wxpur/static/media/talksCardBack.13780cdd.svg"
+  },
+  {
+    "revision": "7af38251cc674bdf03dd30269f15eb2f",
+    "url": "https://mukundakamatham.github.io/csb-wxpur/static/media/www.firstcry.com_.7af38251.png"
+  },
+  {
+    "revision": "43ac718b6d64ccca69b6c5f390c23f72",
+    "url": "https://mukundakamatham.github.io/csb-wxpur/static/media/www.mpl.live_ (1).43ac718b.png"
+  }
+]);
